55653606a04952c0530198580a6dcfa31842fee1
ShivaniPatid <shivanipatidar712@gmail.com>
2023-01-21 10:05:40 +0530

    Create database table using code first approach

---
+++ RepositoryLayer/AppContext/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entities;
+
+namespace RepositoryLayer.AppContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
